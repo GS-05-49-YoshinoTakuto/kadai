a251b1c45dae8f563e89b42e6c5b5009cd8f57aa
taku <t.yoshino.reve@gmail.com>
2016-09-24 18:08:55 +0900

    ジーズ課題9回目 menu.php修正

--- 09/menu.php
+++ 09/menu.php
@@ -21,8 +21,8 @@ if($_SESSION["kanri_flg"]!="1"){
 				<div class="navbar-header">
 					<a class="navbar-brand" href="bm_list_view.php">ブックマーク一覧</a>
 					<a class="navbar-brand" href="bm_insert_view.php">ブックマーク登録</a>
-					<a class="navbar-brand" href="user_insert.php">ユーザー登録</a>
 					<a class="navbar-brand" href="user_select.php">ユーザー一覧</a>
+					<a class="navbar-brand" href="user_insert.php">ユーザー登録</a>
 					<a class="navbar-brand" href="logout.php">ログアウト</a>
 				</div>
 			</div>
